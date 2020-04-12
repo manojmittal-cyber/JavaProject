e657753f8ffa1dba8e59a6fd917bec63e3bd9802
mmitt <mmitt@DESKTOP-85KMBLQ>
2020-04-12 10:31:16 +0530

    Java

--- Test Project/src/Collection2.java
+++ Test Project/src/Collection2.java
@@ -1,8 +1,19 @@
+import java.util.TreeSet;
 
 public class Collection2 
 {
 	public static void main(String[] args) 
 	{
+		TreeSet<String> hs=new TreeSet<String>();
 		
+		hs.add("Manoj");
+		hs.add("Mittal");
+		hs.add("Welcome");
+		hs.add("Welcome");
+		
+		for(String s: hs)
+		{
+			System.out.println(s);
+		}		
 	}
 }
